ddb2641e08589839bb9af18c4ef9b2b961493da4
Cole Bessee <cbessee@uw.edu>
2017-09-12 10:27:24 -0700

    try out overriding fosuser controllers

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new UserBundle\MESAUserBundle()UserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/UserBundle/MESAUserBundle.php
@@ -0,0 +1,14 @@
+// src/UserBundle/MESAUserBundle.php
+<?php
+
+namespace UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MESAUserBundle extends Bundle
+{
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
+}
